8d73aaf3cebe50bc9fe1214d8092917f64c3fe6d
Veronika Srenk <moormie@gmail.com>
2022-12-09 12:21:56 +0100

    add nav bar to HomePage

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,4 +1,4 @@
-import { Grid, Box } from "@mui/material";
+import { Grid, Link, Button } from "@mui/material";
 import { MessageInput } from "../components/MessageInput";
 import { MessageList } from "../components/MessageList";
 import { SideBar } from "../components/SideBar";
@@ -9,16 +9,24 @@ export const HomePage = () => {
   return (
     <>
       <SideBar />
-      <Box
+      <Grid
+        container
         width="calc(100% - 305px)"
         height="calc(100vh - 120px)"
         position="absolute"
         right={0}
-        padding={4}
+        padding={2}
         boxSizing="border-box"
+        rowGap={2}
       >
+        <Grid container item justifyContent="flex-end" bgcolor="white" borderRadius={4} px={2}>
+          <Link component={Button} underline="none" borderRadius="unset" borderBottom="2px solid">Chat</Link>
+          <Link component={Button} underline="none" borderRadius="unset" color="secondary">Contacts</Link>
+          <Link component={Button} underline="none" borderRadius="unset" color="secondary">Settings</Link>
+        </Grid>
         <Grid
           container
+          item
           direction="column"
           bgcolor="white"
           padding={4}
@@ -34,7 +42,7 @@ export const HomePage = () => {
             <MessageInput send={onSendMessage} />
           </Grid>
         </Grid>
-      </Box>
+      </Grid>
     </>
   );
 };
